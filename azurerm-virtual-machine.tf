954a294a0201c8b0e5d2d7f37e5ad3d397f1d4c9
Ray Kao <ray@peopleandcode.com>
2020-08-13 13:12:28 -0400

    initial

--- azurerm-virtual-machine.tf
+++ azurerm-virtual-machine.tf
@@ -1,12 +1,21 @@
+resource "azurerm_public_ip" "admin" {
+  name                = "admin-pip"
+  location            = azurerm_resource_group.admin.location
+  resource_group_name = azurerm_resource_group.admin.name
+  domain_name_label = "${local.prefix}-${terraform.workspace}-${var.location}-admin-machine"
+  allocation_method = "Dynamic"
+}
+
 resource "azurerm_network_interface" "admin" {
   name                = "admin-nic"
   resource_group_name = azurerm_resource_group.admin.name
   location            = azurerm_resource_group.admin.location
 
   ip_configuration {
-    name                          = "internal"
+    name                          = "primary"
     subnet_id                     = azurerm_subnet.admin.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.admin.id
   }
 }
 
@@ -16,6 +25,7 @@ resource "azurerm_linux_virtual_machine" "admin" {
   location            = azurerm_resource_group.admin.location
   size                = "Standard_D4s_v3"
   admin_username      = "${local.prefix}adminuser"
+  
   network_interface_ids = [
     azurerm_network_interface.admin.id,
   ]
